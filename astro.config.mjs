ae5a59409d598f7be2e69bebca936461bc2fc24d
zjwqsd <221502006@smail.nju.edu.cn>
2025-01-18 00:24:41 +0800

    new init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vue from '@astrojs/vue';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://nju-aia.github.io/',
+  integrations: [vue(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  }
+});
